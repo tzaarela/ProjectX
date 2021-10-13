c74130aec7d5956f8aa66ac8eb6c8b0dc23f73af
sloopernine <robin@omnick.se>
2021-10-13 15:39:38 +0200

    added get random enum utility

---
+++ Assets/_ProjectX/Scripts/Utilities/EnumUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Utilites
+{
+    public static class EnumUtils
+    {
+        public static T RandomEnumValue<T>(int startValue)
+        {
+            var values = Enum.GetValues(typeof(T));
+
+            if (startValue > values.Length)
+            {
+                startValue = values.Length;
+            }
+            
+            int random = UnityEngine.Random.Range(startValue, values.Length);
+            return (T)values.GetValue(random);
+        }
+    }
+}
