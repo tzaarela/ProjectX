f8c41cd6085ca9ccc51dbc8a64e7aed219242f0a
Linus-Jonsson <70511818+Linus-Jonsson@users.noreply.github.com>
2021-10-14 13:33:56 +0200

    Added TimeController

---
+++ Assets/_ProjectX/Scripts/UI/TimeController.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts.UI
+{
+	public class TimeController : MonoBehaviour
+	{
+		[SerializeField] private TMP_Text timeText;
+		[SerializeField] private float timeLimit = 100f;
+
+		private float timeRemaining;
+
+		private void Start()
+		{
+			timeRemaining = timeLimit;
+		}
+
+		private void Update()
+		{
+			if (timeRemaining > 0)
+			{
+				timeRemaining -= Time.deltaTime;
+				timeText.text =  Mathf.CeilToInt(timeRemaining).ToString();
+			} else
+			{
+				timeText.text = "Out of time!";
+			}
+		}
+	}
+}
