07a9123240031c139efd1ee66cf7456a938234cc
Linus-Jonsson <70511818+Linus-Jonsson@users.noreply.github.com>
2021-10-19 13:49:12 +0200

    Added Health

---
+++ Assets/_ProjectX/Scripts/Player/Health.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Player
+{
+	public class Health : NetworkBehaviour
+	{
+		[SerializeField] private int startingHealth = 100;
+
+		[SyncVar]
+		private int currentHealth;
+
+		private void Awake()
+		{
+			currentHealth = startingHealth;
+		}
+
+		public void ReceiveDamage()
+		{
+			print($"Player_{GetComponent<NetworkIdentity>().netId} has received damage!");
+		}
+	}
+}
