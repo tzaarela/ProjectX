6a906118b6af336ca0ef0caea962ada104c87cb6
sloopernine <robin@omnick.se>
2021-10-21 15:06:14 +0200

    disable rocket trail when not active

--- Assets/_ProjectX/Scripts/Powerup/Projectiles/Rocket.cs
+++ Assets/_ProjectX/Scripts/Powerup/Projectiles/Rocket.cs
@@ -1,4 +1,5 @@
-﻿using Data.Enums;
+﻿using System;
+using Data.Enums;
 using Managers;
 using Player;
 using UnityEngine;
@@ -7,13 +8,29 @@ namespace PowerUp.Projectiles
 {
 	public class Rocket : ProjectileBase
 	{
+		public TrailRenderer trailerRenderer;
+
+		private void Start()
+		{
+			trailerRenderer.enabled = false;
+		}
+
 		public override void SetupProjectile(Vector3 dir, int netID)
 		{
 			base.SetupProjectile(dir, netID);
+
+			trailerRenderer.enabled = true;
 			
 			rb.AddForce(direction * shootingStrength, ForceMode.Impulse);
 		}
-		
+
+		protected override void OnDisable()
+		{
+			base.OnDisable();
+			
+			trailerRenderer.enabled = false;
+		}
+
 		private void OnCollisionEnter(Collision other)
 		{
 			if(!isServer)
