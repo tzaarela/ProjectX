06e2f8d644559385a44ec3c3e74dc922cf7bf67b
Linus-Jonsson <70511818+Linus-Jonsson@users.noreply.github.com>
2021-10-22 10:55:29 +0200

    Added SmokeFX to HealthStates & Player drops flag on "death"

--- Assets/_ProjectX/Scripts/Player/Health.cs
+++ Assets/_ProjectX/Scripts/Player/Health.cs
@@ -1,4 +1,5 @@
-﻿using Data.Enums;
+﻿using System;
+using Data.Enums;
 using Mirror;
 using UnityEngine;
 
@@ -6,7 +7,10 @@ namespace Player
 {
 	public class Health : NetworkBehaviour
 	{
+		[Header("Settings")]
 		[SerializeField] private int startingHealth = 100;
+
+		[SerializeField] private GameObject[] smokeFX;
 
 		public HealthState currentState = HealthState.Great;
 
@@ -27,38 +31,57 @@ namespace Player
 						+ $"by Player_{attackerId}! (Damage = {damage})");
 
 			currentHealth -= damage;
+			print($"Player_{thisPlayerId} CurrentHealth: " + currentHealth);
 		}
 
 		//SyncVar Hook
 		[Client]
 		private void OnHealthChanged(int oldValue, int newValue)
 		{
+			print("PlayerDamaged CurrentHealth = " + newValue);
+			print("PlayerDamaged HealthState = " + GetHealthState(newValue));
+			
 			if (newValue <= 0)
 			{
 				// ExplosionFX
 				// Respawn
+				print("Player Destroyed!");
+				GetComponent<PlayerController>().DropFlag();
+				currentHealth = 100;
+				currentState = HealthState.Great;
+				smokeFX[0].SetActive(false);
+				smokeFX[1].SetActive(false);
+				smokeFX[2].SetActive(false);
 				return;
 			}
 
 			if (currentState == GetHealthState(newValue))
 				return;
-			
+
 			switch (GetHealthState(newValue))
 			{
 				case HealthState.Great:
 					currentState = HealthState.Great;
-					//No effect?
+					smokeFX[0].SetActive(false);
+					smokeFX[1].SetActive(false);
+					smokeFX[2].SetActive(false);
 					break;
 				case HealthState.Good:
 					currentState = HealthState.Good;
+					smokeFX[0].SetActive(true);
 					//Minor damage
 					break;
 				case HealthState.Ok:
 					currentState = HealthState.Ok;
+					smokeFX[0].SetActive(true);
+					smokeFX[1].SetActive(true);
 					//Medium damage
 					break;
 				case HealthState.Bad:
 					currentState = HealthState.Bad;
+					smokeFX[0].SetActive(true);
+					smokeFX[1].SetActive(true);
+					smokeFX[2].SetActive(true);
 					//Major damage
 					break;
 			}
