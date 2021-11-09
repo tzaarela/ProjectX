534a702d984a7ccb1e8137e5b7d8022056fb1596
sloopernine <robin@omnick.se>
2021-11-09 11:19:11 +0100

    added functions to normalize and denormalize floats

---
+++ Assets/_ProjectX/Scripts/Utilities/FloatUtils.cs
@@ -0,0 +1,15 @@
+﻿namespace Utilites
+{
+	public static class FloatUtils
+	{
+		public static float Normalize(float value, float minValue, float maxValue)
+		{
+			return (value - minValue) / (maxValue - minValue);
+		}
+		
+		public static float Denormalize(float normalizedValue, float minValue, float maxValue) 
+		{
+			return normalizedValue * (maxValue - minValue) + minValue;
+		}
+	}
+}
