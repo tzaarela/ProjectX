55606e74d775ace3a8c8e00b4575946c25acab3a
sloopernine <robin@omnick.se>
2021-11-23 11:09:13 +0100

    should now wait up populate all powerups until all players connected

--- Assets/_ProjectX/Scripts/Managers/PowerupManager.cs
+++ Assets/_ProjectX/Scripts/Managers/PowerupManager.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
+using Data.Containers.GlobalSignal;
 using Data.Enums;
+using Data.Interfaces;
 using Mirror;
 using UnityEngine;
 using Utilites;
 
 namespace Managers
 {
-    public class PowerupManager : NetworkBehaviour
+    public class PowerupManager : NetworkBehaviour, IReceiveGlobalSignal
     {
         private List<PowerupPickup> powerupPickups = new List<PowerupPickup>();
 
@@ -14,8 +16,12 @@ namespace Managers
         [SerializeField] float respawnCooldown;
         private float nextRespawn = 0;
 
+        private bool allPlayersConnected;
+
         private void Start()
         {
+            GlobalMediator.Instance.Subscribe(this);
+            
             GameObject[] respawns;
             respawns = GameObject.FindGameObjectsWithTag("Powerup");
 
@@ -24,13 +30,16 @@ namespace Managers
                 powerupPickups.Add(powerup.GetComponent<PowerupPickup>());
             }
         }
-
+        
         // [Server]
         private void Update()
         {
             if (!isServer)
                 return;
 
+            if (!allPlayersConnected)
+                return;
+            
             if (nextRespawn > Time.time)
                 return;
 			
@@ -52,6 +61,14 @@ namespace Managers
         private PowerupType PickPowerupType()
         {
             return EnumUtils.RandomEnumValue<PowerupType>(0);
+        }
+
+        public void ReceiveGlobal(GlobalEvent eventState, GlobalSignalBaseData globalSignalData = null)
+        {
+            if (eventState == GlobalEvent.ALL_PLAYERS_CONNECTED_TO_GAME)
+            {
+                allPlayersConnected = true;
+            }
         }
     }
 }
